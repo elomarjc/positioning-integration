64b31627fd9f288c59a3a46148b40673fda19a33
elomarjc <elomarjc@gmail.com>
2022-10-18 10:15:32 +0200

    Added info regarding tagID and setSpeed

--- prediction/robotHumanClass.py
+++ prediction/robotHumanClass.py
@@ -9,14 +9,14 @@ class robot: # finalXY -> self.move_x_entry.get()
         self.currentYcoord = currentYcoord
         self.finalXcoord = finalXcoord
         self.finalYcoord = finalYcoord
-        self.absolutMaxSpeed = absolutMaxSpeed
-        self.absolutMinSpeed = absolutMinSpeed
+        self.absolutMaxSpeed = absolutMaxSpeed #use method "set_robot_speed" under controller.py line 217
+        self.absolutMinSpeed = absolutMinSpeed #use method "set_robot_speed" under controller.py line 217
 
-    def setSpeed (self):
+    def setSpeed (self):   #use method "set_robot_speed" under controller.py line 217
         pass
 
 class human:
-    def __init__ (self, tagID, currentXcoord, currentYcoord):
+    def __init__ (self, tagID, currentXcoord, currentYcoord): # tagID -> tag_id_from_json
         self.tagID = tagID
         self.currentXcoord = currentXcoord
         self.currentYcoord = currentYcoord
